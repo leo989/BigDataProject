e6ed7d33fe1925fc98265d6d1b2b0a894babdf48
valerio <valerio29@gmail.com>
2014-06-16 10:48:10 +0200

    changed some bits on index

--- ProgettoBigData/war/javascript/myscript.js
+++ ProgettoBigData/war/javascript/myscript.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	$('#search-btn').click(search);
+	$('#type-of-search').change(function(){
+		if (this.value === '3') {
+			$('#best-total-cost').fadeIn();
+		} else {
+			$('#best-total-cost').hide();
+		}
+	})
 })
 
 var search = function() {
